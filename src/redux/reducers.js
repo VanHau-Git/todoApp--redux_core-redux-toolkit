c0e762545fff517f03b105aefe4ec7c27107479b
VanHau-Git <tranhau.010120199@gmail.com>
2023-03-15 23:15:19 +0700

    delete-file-reducer

--- src/redux/reducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import FiltersReducer from "../components/Filter/filtersSlice";
-import TodoListReducer from "../components/TodoList/todosSlice";
-
-const rootReducer = combineReducers({
-  filters: FiltersReducer,
-  todoList: TodoListReducer
-})
-export default rootReducer;
